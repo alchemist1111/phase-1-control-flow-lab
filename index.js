276742a8c7e1c8d2d426d8dd3dade211ced1f60f
eugene-emoyo-ojakaa <emoyoeugine@gmail.com>
2024-03-18 21:29:15 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!'; 
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipType){
   // Write your code here!
+    switch (tipType) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
 }
